a9c91f10e42843cec924a85a202975aa07cea2b9
Maksim Yurkov <yurkov.maksim.sergeevich@gmail.com>
2019-01-09 11:08:08 +0300

    Update ht-elements-statistics-payout.js
    change docs url

--- ht-elements-statistics-payout.js
+++ ht-elements-statistics-payout.js
@@ -82,7 +82,7 @@ class HTElementsStatistics extends LitElement {
           : html`
           <div id="info">
             <h2>Создание заказа на выплату</h2>
-            <p>Выплаты осуществляются с 1 по 10 число месяца. (<a href="https://docs.elements.01.ht/for-authors/payments/" target="_blank" rel="noopener">о выплатах</a>)</p>
+            <p>Выплаты осуществляются с 1 по 10 число месяца. (<a href="https://docs.elements.01.ht/guide/payout/#наnоги-и-страховые-взносы" target="_blank" rel="noopener">о выплатах</a>)</p>
             <div class="item"><iron-icon icon="ht-elements-statistics-payout:${
               ready ? "check" : "close"
             }"></iron-icon><div><a href="/account/payout">Настройки выплат</a> указаны</div>
